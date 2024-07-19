0950bf4227019e708b25cf2cec60cbd530be9b1f
Beno!t POLASZEK <bpolaszek@gmail.com>
2024-07-19 16:51:49 +0200

    feat: use state options as suggested by @soyuka

--- src/Entity/BookOutput.php
+++ src/Entity/BookOutput.php
@@ -2,6 +2,17 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiResource;
+use App\State\BookProvider;
+
+#[ApiResource(
+    shortName: 'Book',
+    input: Book::class,
+    output: BookOutput::class,
+    provider: BookProvider::class,
+    stateOptions: new Options(Book::class),
+)]
 final class BookOutput
 {
     public int $id;
--- tests/StateProviderTest.php
+++ tests/StateProviderTest.php
@@ -4,21 +4,36 @@ namespace App\Tests;
 
 it('creates a book', function () {
     $book = api()->post('/api/books', ['name' => 'PHP for dummies'])->toArray();
+    dump(['POST /api/books' => $book]);
 
     expect($book['name'])->toBe('PHP for dummies') // <-- OK
     ->and($book['id'])->toBe(1) // <-- OK
-    ->and($book['@id'])->toBe('/api/books/1') // <-- OK
     ->and($book['@type'])->toBe('Book') // <-- OK
+    ->and($book['@id'])->toBe('/api/books/1') // <-- Failed, got '/api/.well-known/genid/somerandomidentifier'
     ->and($book['type'])->toBe('output'); // <-- Failed, got 'entity'
 });
 
 it('lists books', function () {
     $books = api()->get('/api/books')->toArray();
+    dump(['GET /api/books' => $books]);
+
     $book = $books['hydra:member'][0];
 
     expect($book['name'])->toBe('PHP for dummies') // <-- OK
     ->and($book['id'])->toBe(1) // <-- OK
     ->and($book['type'])->toBe('output') // <-- OK
-    ->and($book['@type'])->toBe('Book') // <-- Failed, got 'BookOutput'
-    ->and($book['@id'])->toBe('/api/books/1'); // <-- Failed, got '/api/.well-known/genid/somerandomidentifier'
+    ->and($book['@type'])->toBe('Book') // OK
+    ->and($book['@id'])->toBe('/api/books/1'); // OK
+});
+
+
+it('gets a book', function () {
+    $book = api()->get('/api/books/1')->toArray();
+    dump(['GET /api/books/1' => $book]);
+
+    expect($book['name'])->toBe('PHP for dummies') // <-- OK
+    ->and($book['id'])->toBe(1) // <-- OK
+    ->and($book['@id'])->toBe('/api/books/1') // <-- OK
+    ->and($book['@type'])->toBe('Book') // <-- OK
+    ->and($book['type'])->toBe('output'); // <-- OK
 });
